66c66c9766966172e3ab85fe18923fe5d9d9cbce
karolsobon <karolsobon@aol.com>
2024-09-04 21:38:28 +0200

    first commiy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Button } from 'react-bootstrap';  
+import NavigationBar from './components/Navbar';
+import AddProduct from './components/AddProduct';
+import EditProduct from './components/EditProduct';
+import DeleteProduct from './components/DeleteProduct';
+import Login from './components/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const loggedIn = localStorage.getItem('isLoggedIn') === 'true';
+        setIsLoggedIn(loggedIn);
+    }, []);
+
+    const handleLogin = () => {
+        setIsLoggedIn(true);
+        localStorage.setItem('isLoggedIn', 'true');
+    };
+
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+        localStorage.removeItem('isLoggedIn'); // Usuń stan logowania
+    };
+
+    return (
+        <Router>
+            {isLoggedIn ? (
+                <>
+                    <NavigationBar onLogout={handleLogout} /> {/* Przekazujemy funkcję wylogowania */}
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/add" />} />
+                        <Route path="/add" element={<AddProduct />} />
+                        <Route path="/edit" element={<EditProduct />} />
+                        <Route path="/delete" element={<DeleteProduct />} />
+                    </Routes>
+                </>
+            ) : (
+                <Login onLogin={handleLogin} />
+            )}
+        </Router>
+    );
 }
 
 export default App;
